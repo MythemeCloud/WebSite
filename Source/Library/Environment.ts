5602f3bd53458e03d3b7c817ffd5ed85efad2c96
Nikola Hristov <nikola@nikolahristov.tech>
2023-10-14 13:48:05 +0300

    squash!

--- Source/Library/Environment.ts
+++ Source/Library/Environment.ts
@@ -2,14 +2,10 @@
 
 export default Zod.z
 	.object({
-<<<<<<< HEAD
 		AUTH_SECRET: z.string().default(""),
 		AUTH_TRUST_HOST: z.string().default(""),
 		GITHUB_ID: z.string().default(""),
 		GITHUB_SECRET: z.string().default(""),
-=======
-		Token: Zod.z.string().default(""),
->>>>>>> upstream/main
 	})
 	.parse(process.env);
 
